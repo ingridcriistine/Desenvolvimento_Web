e7349b871ebd5244dc3321e9c9abd6e75083958b
Ingrid Rocha <104801948+ingridcriistine@users.noreply.github.com>
2024-08-13 11:40:02 -0300

    Create script7.js

---
+++ Testes/script7.js
@@ -0,0 +1,8 @@
+function Sum() {
+    let n1 = parseInt(document.getElementById("n1").value)
+    let n2 = parseInt(document.getElementById("n2").value)
+    let soma = 0
+    soma = n1 + n2;
+    alert("A soma dos números é igual a " + soma)
+    console.log("Resultado da soma: " + soma)
+}
